8e46666fb6921a12a09804bc39450490b90d3cc5
sturple <shawn.turple@shaw.ca>
2016-12-11 15:17:29 -0800

    Created function getRouter() to check if route exists. Starting to add session logic for auth. Adding proper response headers and intigration of twig template render.

--- src/TurpEdit/Common/Uri.php
+++ src/TurpEdit/Common/Uri.php
@@ -1,17 +1,25 @@
 <?php
 namespace TurpEdit\Common;
 
+use Symfony\Component\EventDispatcher\Event;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class Uri 
 {
     
     var $content ='';
+    var $container;
+    var $request = null, $session=null;
+    var $twig;
+    var $content_type = 'text/html';
     var $site_routes = [
         [
             'route'     => '_ajax',
@@ -22,47 +30,101 @@ class Uri
             'route'     => '_settings',
             'path'      => '/settings/',
             'callback'  => 'TurpEdit\Common\Uri::actionSettings'
+        ],
+        [
+            'route'     => '_login',
+            'path'      => '/telogin/',
+            'callback'  => 'TurpEdit\Common\Uri::actionLogin'
         ]
     ];
 
-    public function __construct()
+    public function __construct($container)
     {    
-        //define routes
-        $routes = new RouteCollection();
-        foreach ($this->site_routes as $route){
-            $routes->add(
-                $route['route'],
-                new Route(
-                    $route['path'],
-                    array('controller'=>$route['callback'])
-            ));
+
+        $this->container = $container;
+        $this->request = Request::createFromGlobals();
+        $this->session = $this->container['session'];
+        $this->twig = $this->container['twig'];
+        // Check if Authenticated.
+        if (($this->session->has('user')) or true){
+            $this->getRoute();
+        }
+        else {
+            echo ' has no session';
+            //$this->container['session']->set('user','admin');
         }
 
-        $request = Request::createFromGlobals();
-        $context = new RequestContext();
-        $context->fromRequest($request);
-        try {
-            $matcher = new UrlMatcher($routes, $context);      
-            $p = $matcher->match($request->getPathInfo());
-            call_user_func($p['controller']);
-        }
-        catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e){
-            print_R($e->getMessage());
-        }
        
     }
     
-    
-    public function actionAjax(){
-        $this->content = 'actionajax';
+    /*
+    * Logic to determine if valid Route
+    */
+    private function getRoute(){
+         //define routes
+         $routes = new RouteCollection();
+         foreach ($this->site_routes as $route){
+             $routes->add(
+                 $route['route'],
+                 new Route(
+                     $route['path'],
+                     array('controller'=>$route['callback'])
+            ));
+        }
+        // Getting Request and checking to see if they match
+        
+        $context = new RequestContext();
+        $context->fromRequest($this->request);
+        try {
+            $matcher = new UrlMatcher($routes, $context);      
+            $p = $matcher->match($this->request->getPathInfo());
+            //calls user function
+            call_user_func($p['controller']);
+        }
+        catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e){
+            $this->container['dispatcher']->dispatch('router.ResourceNotFounndException' );
+            print_R($e->getMessage());
+        }                    
     }
     
-    public function actionSettings(){
+    /***
+     * 
+     * Route Actions 
+     * 
+     ***/
+    private function actionAjax(){
+        $data = array('message'=>'ajaxapplication');
+        $data = $this->session->all();
+        $this->content_type = 'application/json';
+        $this->content = json_encode($data);;
+    }
+    
+    private function actionSettings(){
         $this->content = 'actionsettings';
     }
     
-    public function getRoute(){
-
-        return $this->content;
+    private function actionLogin() {
+        $data = [
+            'content' => 'Login Page'
+        ];
+        $this->content = $this->twig->render('login.twig',$data);
     }
+    
+    
+    /***
+     * 
+     * Response
+     *
+     ***/
+    public function response(){
+        $response = new Response(
+            'Content',
+            Response::HTTP_OK,
+            array('content-type'=> $this->content_type)
+        );
+        $response->setCharset('utf8');
+        $response->setContent($this->content);
+        $response->send();
+    }
+    
 }
