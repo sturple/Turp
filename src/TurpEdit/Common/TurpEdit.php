66b836e7ed268b7c4f466c1aad61b502cfad1c65
sturple <shawn.turple@shaw.ca>
2016-12-11 15:13:29 -0800

    Added Logging, Session, Dispatcher, and Twig.  Updated URI to add container

--- src/TurpEdit/Common/TurpEdit.php
+++ src/TurpEdit/Common/TurpEdit.php
@@ -3,8 +3,17 @@
 namespace TurpEdit\Common;
 
 use Pimple\Container;
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 use TurpEdit\Common\Uri;
 use TurpEdit\Common\Configuration;
+use TurpEdit\Common\Event\TurpSubscriber;
 
 class TurpEdit extends Container
 {
@@ -27,23 +36,43 @@ class TurpEdit extends Container
     protected static function load(array $values)
     {
         $container = new static($values);
-        
+
         $container['editor']  = $container;
+        
+        // session
+        $container['session'] = function($c) {
+            $session = new Session();
+            $session->start();
+            return $session;
+        };            
+        
+        //monolog/logger setup
+        $container['log'] = new Logger('TurpEdit');
+        $container['log']->pushHandler(new StreamHandler(LOG_DIR.'error.log', Logger::DEBUG));
+        
+        // event dispacher;
+        $container['dispatcher'] = new EventDispatcher();
+        $container['dispatcher']->addSubscriber(new TurpSubscriber());
+        
         // router
         $container['config'] = function($c) {
-            return new Configuration();
+            return new Configuration($c);
+        };
+        $container['twig'] = function($c) {
+            $path = ROOT_DIR.$c['config']->config->value('twig.path');
+            $loader = new \Twig_Loader_Filesystem($path);
+            $params = array();
+            return  new \Twig_Environment($loader, $params);
+            
         };
         $container['uri'] = function($c) {
-          return new Uri();
+          return new Uri($c);
         };
         // user
         $container['user'] = function($c) {
             return new User(); 
         };
-        // session
-        $container['session'] = function($c) {
-            return array();
-        };
+
         // projects
         $container['projects'] = function ($c) {
             return array();
@@ -51,13 +80,14 @@ class TurpEdit extends Container
         $container['plugins'] = function ($c) {
             return array(); 
         };
+        $container['log']->warning('Test');
         
         return $container;
     }
     
     public function process() {
         $this['config']->init();
-        return ($this['uri']->getRoute());
+        $this['uri']->response();
         
         
     }
