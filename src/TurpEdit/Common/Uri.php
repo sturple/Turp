26dab19d3c238cd4c804550325c30f6b8f638310
sturple <shawn.turple@shaw.ca>
2016-12-14 22:36:36 -0800

    Updated routing authentication, and adding param to check if requires auth. Added home and logout routes. Modifications to Router function.

--- src/TurpEdit/Common/Uri.php
+++ src/TurpEdit/Common/Uri.php
@@ -4,6 +4,7 @@ namespace TurpEdit\Common;
 use Symfony\Component\EventDispatcher\Event;
 
 use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
@@ -19,42 +20,49 @@ class Uri
     var $container;
     var $request = null, $session=null;
     var $twig;
+    var $routeparams=array();
     var $content_type = 'text/html';
     var $site_routes = [
         [
+            'route'     => '_home',
+            'path'      => '/',
+            'auth'      => true,
+            'callback'  => 'TurpEdit\Common\Uri::actionHome'
+        ],
+        [
             'route'     => '_ajax',
             'path'      => '/ajax/',
+            'auth'      => true,
             'callback'  => 'TurpEdit\Common\Uri::actionAjax'
         ],
         [
             'route'     => '_settings',
             'path'      => '/settings/',
+            'auth'      => true,
             'callback'  => 'TurpEdit\Common\Uri::actionSettings'
         ],
         [
             'route'     => '_login',
             'path'      => '/telogin/',
+            'auth'      => false,
             'callback'  => 'TurpEdit\Common\Uri::actionLogin'
+        ], 
+        [
+            'route'     => '_logout',
+            'path'      => '/logout/',
+            'auth'      => false,
+            'callback'  => 'TurpEdit\Common\Uri::actionLogout'
         ]
     ];
 
-    public function __construct($container)
+    public function __construct()
     {    
 
-        $this->container = $container;
+        $this->container = \TurpEdit\Common\TurpEdit::instance();
         $this->request = Request::createFromGlobals();
         $this->session = $this->container['session'];
         $this->twig = $this->container['twig'];
-        // Check if Authenticated.
-        if (($this->session->has('user')) or true){
-            $this->getRoute();
-        }
-        else {
-            echo ' has no session';
-            //$this->container['session']->set('user','admin');
-        }
-
-       
+        $this->getRoute();
     }
     
     /*
@@ -64,11 +72,16 @@ class Uri
          //define routes
          $routes = new RouteCollection();
          foreach ($this->site_routes as $route){
+             
              $routes->add(
                  $route['route'],
                  new Route(
                      $route['path'],
-                     array('controller'=>$route['callback'])
+                     array(
+                        'controller'=>$route['callback'],
+                        'auth'      =>$route['auth']
+                     )
+                     
             ));
         }
         // Getting Request and checking to see if they match
@@ -77,9 +90,17 @@ class Uri
         $context->fromRequest($this->request);
         try {
             $matcher = new UrlMatcher($routes, $context);      
-            $p = $matcher->match($this->request->getPathInfo());
-            //calls user function
-            call_user_func($p['controller']);
+            $this->routeparams = $matcher->match($this->request->getPathInfo());
+            
+            // Check if Authenticated by session of User or if auth is not required.
+            if ( ($this->session->has('user') and $this->session->get('user')->checkAuthentication()) or !$this->routeparams['auth'] ){
+                //calls user function
+                call_user_func($this->routeparams['controller']);
+            }
+            else {
+                $this->actionLogin();
+            }
+            
         }
         catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e){
             $this->container['dispatcher']->dispatch('router.ResourceNotFounndException' );
@@ -92,9 +113,11 @@ class Uri
      * Route Actions 
      * 
      ***/
+     
+    private function actionHome() {
+       $this->content = $this->twig->render('index.twig',$this->getTwigData());
+    }
     private function actionAjax(){
-        $data = array('message'=>'ajaxapplication');
-        $data = $this->session->all();
         $this->content_type = 'application/json';
         $this->content = json_encode($data);;
     }
@@ -106,8 +129,26 @@ class Uri
     private function actionLogin() {
         $data = [
             'content' => 'Login Page'
-        ];
-        $this->content = $this->twig->render('login.twig',$data);
+        ];        
+        if ($this->request->get('csrf') == $this->session->get('csrftoken')){
+            $this->session->remove('csrftoken');
+            $this->session->remove('csrftoken2');
+            $redirect = $this->container['user']->authenticateUser($this->request);
+        }
+        $this->content = $this->twig->render('login.twig',$this->getTwigData($data));
+    }
+    
+    private function actionLogout($p) {
+        //clears all session data
+        $this->container['session']->invalidate();
+        $this->actionLogin();
+    }
+    
+    private function getTwigData($data=array()){
+        $data['settings']  = $this->container['settings'];
+        $data['user'] = $this->container['session']->get('user');  
+        $data = array_merge($data,$this->routeparams);
+        return $data;
     }
     
     
@@ -116,7 +157,7 @@ class Uri
      * Response
      *
      ***/
-    public function response(){
+    public function res(){
         $response = new Response(
             'Content',
             Response::HTTP_OK,
@@ -125,6 +166,7 @@ class Uri
         $response->setCharset('utf8');
         $response->setContent($this->content);
         $response->send();
+        
     }
     
 }
